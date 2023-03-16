5a85b938409f22b00c70603604dc392feee3165d
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-03-16 09:59:10 +0900

    type추가 -> 오류 해결

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -14,6 +14,12 @@ public class Main {
         TestBean1 obj2 = ctx.getBean("t2", TestBean1.class);
         obj2.prData();
 
+        TestBean1 obj3 = ctx.getBean("t3", TestBean1.class);
+        obj3.prData();
+
+        TestBean1 obj4 = ctx.getBean("t4", TestBean1.class);
+        obj4.prData();
+
         ctx.close();
     }
 }
--- src/main/java/kr/hs/study/beans/TestBean1.java
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -17,15 +17,29 @@ public class TestBean1 {
 
     public void prData() {
         System.out.println("data1: "+data1);
-        System.out.println("data1: "+data2);
-        System.out.println("data1: "+data3);
-        System.out.println("==========================");
+        System.out.println("data2: "+data2);
+        System.out.println("data3: "+data3);
+        System.out.println("==========================\n");
     }
 
     public TestBean1(int data1) {   // 메서드 오버로딩
         System.out.println("===TestBean1의 int data1===");
         this.data1 = data1;
         this.data2 = 1.1;
-        this.data3="spring1";
+        this.data3 = "spring1";
+    }
+
+    public TestBean1(double data2) {   // 메서드 오버로딩
+        System.out.println("==TestBean1의 double data2==");
+        this.data1 = 2;
+        this.data2 = data2;
+        this.data3 = "spring2";
+    }
+
+    public TestBean1(String data3) {   // 메서드 오버로딩
+        System.out.println("==TestBean1의 String data3==");
+        this.data1 = 3;
+        this.data2 = 3.3;
+        this.data3 = data3;
     }
 }
