0b1c7b3efa842ec6f7f91d6f3c2fe17093273bc3
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-03-16 10:22:16 +0900

    constructor ref 2가지 방법

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,24 +1,30 @@
 package kr.hs.study;
 
 import kr.hs.study.beans.TestBean1;
+import kr.hs.study.beans.TestBean2;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
-        TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+//        TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+//        obj1.prData();
+//
+//        TestBean1 obj2 = ctx.getBean("t2", TestBean1.class);
+//        obj2.prData();
+//
+//        TestBean1 obj3 = ctx.getBean("t3", TestBean1.class);
+//        obj3.prData();
+//
+//        TestBean1 obj4 = ctx.getBean("t4", TestBean1.class);
+//        obj4.prData();
+
+        TestBean2 obj1 = ctx.getBean("t5", TestBean2.class);
         obj1.prData();
 
-        TestBean1 obj2 = ctx.getBean("t2", TestBean1.class);
+        TestBean2 obj2 = ctx.getBean("t6", TestBean2.class);
         obj2.prData();
-
-        TestBean1 obj3 = ctx.getBean("t3", TestBean1.class);
-        obj3.prData();
-
-        TestBean1 obj4 = ctx.getBean("t4", TestBean1.class);
-        obj4.prData();
 
         ctx.close();
     }
